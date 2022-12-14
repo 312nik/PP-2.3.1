f770dd7498b0179d6b840b016f67ab412c37f33c
Moiseev Nikolay <niko-light@bk.ru>
2022-12-14 09:28:59 +0300

    исправлено

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import web.model.User;
 
 import java.util.List;
 @Service
-@Transactional
+@Transactional (readOnly = true)
 public class UserServiceImpl implements UserService{
 
     private final UserDAO userDAO;
@@ -15,28 +15,28 @@ public class UserServiceImpl implements UserService{
     public UserServiceImpl(UserDAO userDAO) {
         this.userDAO = userDAO;
     }
-   @Transactional(readOnly = true)
+
     @Override
     public List<User> findAllUsers() {
         return userDAO.findAllUsers();
     }
-    @Transactional(readOnly = true)
+
     @Override
     public User findById(Long id) {
         return userDAO.findById(id);
     }
-
+    @Transactional
     @Override
     public void saveUser(User user) {
         userDAO.saveUser(user);
 
     }
-
+    @Transactional
     @Override
     public void updateUser(Long id, User updateUser) {
         userDAO.updateUser(id,updateUser);
     }
-
+    @Transactional
     @Override
     public void deleteUser(Long id) {
         userDAO.deleteUser(id);
